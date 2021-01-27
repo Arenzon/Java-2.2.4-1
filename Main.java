571424ef564b79abd3349b950571a6694f67f64e
Natalya <76731572+Arenzon@users.noreply.github.com>
2021-01-27 23:21:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main ( String[] args ) {
         BonusMilesService service = new BonusMilesService();
         long miles = service.calculate(10_000);
         System.out.println("Мили " + miles);
