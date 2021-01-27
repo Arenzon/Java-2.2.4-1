b4f4cb06c00c453a107b3e1bf98920cb0da18a27
Natalya <76731572+Arenzon@users.noreply.github.com>
2021-01-27 23:20:08 +0300

    Update BonusMilesService.java

--- BonusMilesService.java
+++ BonusMilesService.java
@@ -1,7 +1,7 @@
-public class Main {
-    public static void main ( String[] args ) {
-        BonusMilesService service = new BonusMilesService();
-        long miles = service.calculate(10_000);
-        System.out.println("Мили " + miles);
+public class BonusMilesService {
+    public long calculate ( int cost ) {
+        int price = 20;
+        long miles = cost / price;
+        return miles;
     }
 }
